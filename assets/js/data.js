a66c7b17028d426f12bb64fbc6aa8340ceff05f1
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-01-12 03:31:06 -0500

    demo

---
+++ assets/js/data.js
@@ -0,0 +1,5 @@
+const data = {
+    "title": "AccessPoint de Doble banda, 802.11a / b / g / n / ac, MIMO3x3, tres Antenas de doble banda de 2, 4 GHz, 5 GHz - 3 dBi, 02 puerto 10/100/1000, UBiQuiTi - MODELO: UAP-AC - PRO"
+}
+
+export default data
